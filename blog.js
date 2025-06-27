0a3c32b825d85f2286452b73624965c76052d199
Damini Rajoria <rajoriadamini@gmail.com>
2025-06-27 15:20:10 +0530

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+
+let blogSchema = new mongoose.Schema(
+{
+    title:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    author:{
+        type:String,
+        trim:true,
+        required:true
+    },
+
+    Comment:{
+        type:String,
+        trim:true,
+        required:true
+    },
+}
+
+)
+
+const Blog = mongoose.model('Blog',blogSchema)
+
+module.exports = Blog;
